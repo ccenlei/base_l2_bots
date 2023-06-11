77332b09be0aaa88f9d1993d315fb5c615f33a0e
ccenlei <iacc7441@yahoo.co.jp>
2023-06-11 19:26:42 +0900

    add db fetch accounts way.

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 
 from concurrent.futures import ThreadPoolExecutor
 import json
+import pymysql
 
 from dackie_dapp.dackie_harvester import dackie_harvest
 from dackie_dapp.dackie_swapper import dackieswap_token2eth
+
+
+# db: from database; file: from accounts.json
+acc_model = 'db'
 
 
 # auto harvest dackie farms.
@@ -21,10 +26,25 @@ def dapp_bot(thread_id: int, name: str, key: str):
 
 if __name__ == '__main__':
     print('start to interact base l2 dapps.')
-    file_path = 'accounts.json'
-    with open(file_path, mode='r') as file:
-        accounts = json.load(file)
-    on_accounts = [account for account in accounts if account['is_on'] is True]
+    match acc_model:
+        case 'db':
+            with pymysql.connect(host='localhost', port=3306, user='ccenlei', password='123', database='web3') as db:
+                cursor = db.cursor()
+                sql = 'select name,address,pri_key from eth_account where is_on = 0'
+                cursor.execute(sql)
+                rows = cursor.fetchall()
+                on_accounts = []
+                for row in rows:
+                    acc_dic = {'name': row[0], 'addr': row[1], 'key': row[2]}
+                    on_accounts.append(acc_dic)
+        case 'file':
+            file_path = 'accounts.json'
+            with open(file_path, mode='r') as file:
+                accounts = json.load(file)
+                on_accounts = [account for account in accounts if account['is_on'] == 0]
+        case _:
+            print('select a way of accounts load.')
+            exit(-1)
     index = 0
     with ThreadPoolExecutor(max_workers=10) as pool:
         for on_account in on_accounts:
