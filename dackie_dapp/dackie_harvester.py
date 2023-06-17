1bc80dfa1c3ee3d9b542ef1d7cb3946b5fc939e2
ccenlei <iacc7441@yahoo.co.jp>
2023-06-17 14:22:02 +0900

    update

--- dackie_dapp/dackie_harvester.py
+++ dackie_dapp/dackie_harvester.py
@@ -14,7 +14,7 @@ w3 = Web3(Web3.HTTPProvider('https://svc.blockdaemon.com/base/testnet/native/htt
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 
-# Dackie Farms earn : Stake LP tokens to earn.
+# ====================================Dackie Farms earn : Stake LP tokens to earn.====================================
 farms_addr = '0xDB8726189978d09D8c8A449Eda6c72A1e2EB228e'
 farms_contract = get_contract_http(farms_addr)
 
@@ -40,9 +40,10 @@ def farms_harvest(key: str, pid=14):
             'nonce': w3.eth.get_transaction_count(account.address),
         })
     sign_tx(tx_dict, account, 'dackie farms harvest')
+# ====================================Dackie Farms earn : Stake LP tokens to earn.====================================
 
 
-# Dackie Pools earn : Just stake some tokens to earn.
+# ====================================Dackie Pools earn : Just stake some tokens to earn.====================================
 dackie_token = '0xcf8E7e6b26F407dEE615fc4Db18Bf829E7Aa8C09'
 pools_weth = '0x70249aF497f2040c0677f5Ea1B0dB2595f94803F'
 pools_wbtc = '0xd2BC5Bd918779264aD08cfaC8A276dC6e96AF517'
@@ -84,3 +85,4 @@ def pools_harvest(key: str, type='weth'):
             'nonce': w3.eth.get_transaction_count(account.address),
         })
     sign_tx(tx_dict, account, 'dackie pools harvest')
+# ====================================Dackie Pools earn : Just stake some tokens to earn.====================================
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import json
 from typing import Any
 import pymysql
 
-from dackie_dapp.dackie_harvester import farms_harvest
+from dackie_dapp.dackie_harvester import farms_harvest, pools_harvest
 from dackie_dapp.dackie_swapper import dackieswap_token2eth
 from oil_dapp.oil_harvester import oil_harvest
 
@@ -15,14 +15,12 @@ from oil_dapp.oil_harvester import oil_harvest
 # db: from database; file: from accounts.json
 acc_model = 'db'
 
-
 def file_load_accs():
     file_path = 'accounts.json'
     with open(file_path, mode='r') as file:
         accounts = json.load(file)
         on_accounts = [account for account in accounts if account['is_on'] == 0]
     return on_accounts
-
 
 def db_load_accs():
     with pymysql.connect(host='localhost', port=3306, user='ccenlei', password='123', database='web3') as db:
@@ -37,33 +35,38 @@ def db_load_accs():
     return on_accounts
 
 
+# ====================================auto harvest dackie.====================================
 pids = (13, 14)
 
-
-# auto harvest dackie farms.
-def auto_dackie_harvest_bot(key: str):
+def dackie_farms_bot(key: str):
     for pid in pids:
         farms_harvest(key, pid)
-    dackieswap_token2eth(key, '0xcf8E7e6b26F407dEE615fc4Db18Bf829E7Aa8C09')
+    # dackieswap_token2eth(key, '0xcf8E7e6b26F407dEE615fc4Db18Bf829E7Aa8C09')
+
+types = ('weth',)
+
+def dackie_pools_bot(key: str):
+    for type in types:
+        pools_harvest(key, type)
+# ====================================auto harvest dackie.====================================
 
 
+# ====================================auto harvest oil farms.====================================
 nft_token_ids = (0,2)
 
-
-# auto harvest oil farms.
-def auto_oil_harvest_bot(key: str):
+def oil_harvest_bot(key: str):
     for token_id in nft_token_ids:
         oil_harvest(key, token_id)
-    dackieswap_token2eth(key, '0x5bC8BDC70D7cD2ff78E0CDA60d326685c047f7B5')
-
+    # dackieswap_token2eth(key, '0x5bC8BDC70D7cD2ff78E0CDA60d326685c047f7B5')
+# ====================================auto harvest dackie.====================================
 
 
 # bot thread.
 def dapp_bot(thread_id: int, name: str, key: str):
     print(f"start to bot: id={thread_id}, name={name}")
-    auto_dackie_harvest_bot(key)
-    auto_oil_harvest_bot(key)
-
+    dackie_farms_bot(key)
+    dackie_pools_bot(key)
+    # oil_harvest_bot(key)
 
 def pool_excutes():
     print('start to interact base l2 dapps.')
